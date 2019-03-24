8045f796bc58a973571557e368ca648e6257db9e
Brett Dawidowski <brett@dawi.io>
2019-03-24 18:33:57 -0400

    [Max Char] #4 Completed

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,49 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+      const charMap = {};
+
+      let max = 0;
+      let maxChar = '';
+
+      for (let char of str) {
+            // 1.0 Single Iteration
+            charMap[char] = charMap[char] + 1 || 1;
+            
+            if (charMap[char] > max) {
+                  max = charMap[char];
+                  maxChar = char;
+            }
+
+            // 2.1 Short Form Option
+            /**
+                  charMap[char] = charMap[char] + 1 || 1;
+            **/
+            
+            // 2.2 Long Form Option
+            /** 
+                  if (charMap[char]) {
+                        charMap[char]++
+                  } else {
+                        charMap[char] = 1;
+                  }
+            **/
+      }
+
+      // 2.1 or 2.2 Secondary Loop
+      /**
+            for (let char in charMap) {
+                  if (charMap[char] > max) {
+                        max = charMap[char];
+                        maxChar = char;
+                  }
+            }
+      **/
+
+
+      return maxChar;
+
+}
 
 module.exports = maxChar;
