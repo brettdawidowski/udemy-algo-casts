e02018695ec288c6a3b0f75175a4fc2097e64568
Brett Dawidowski <brett@dawi.io>
2019-03-25 22:49:51 -0400

    [Capitalize] #8 Completed

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,12 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+      const words = str.split(' ');
+
+      return words.map((word) => {
+            return word[0].toUpperCase() + word.slice(1)
+      }).join(' ');
+}
 
 module.exports = capitalize;
