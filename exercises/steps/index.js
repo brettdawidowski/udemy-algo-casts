b7b3217d4bfbe67b934326557977b386b12217ac
Brett Dawidowski <brett@dawi.io>
2019-03-26 21:55:53 -0400

    [Steps] #9 Completed

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,32 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n, s) {
+      // Start Recursive Function at first step
+      // Takes current step and total num of steps
+      // Defaults param s to 1
+      s = s || 1;
+      // Repeats # * current step num and repeats ' ' * total steps - current step
+      console.log('#'.repeat(s) + ' '.repeat(n - s));
+      // Calls itself if more steps to climb and increase current step by 1
+      if (s < n) steps(n, s + 1)
+}
+
+// function steps(n) {
+//       // Starting at 1 build each step
+//       for (let stepNum = 1; stepNum <= n; stepNum++) {
+//             // Init string for step
+//             let stepOutput = '';
+//             // Duplicate # based on current step num
+//             for (let poundSign = 1; poundSign <= stepNum; poundSign++) {
+//                   stepOutput += '#';
+//             }
+//             // Add ' ' to end based on total - current step num
+//             for (let space = stepNum; space < n; space++) {
+//                   stepOutput += ' ';
+//             }
+//             console.log(stepOutput);
+//       }
+// }
 
 module.exports = steps;
