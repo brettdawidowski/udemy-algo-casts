881f793909d784123d3995538a59210bf5aa6a9c
Brett Dawidowski <brett@dawi.io>
2019-04-04 00:06:43 -0400

    [Recursion with Memoize] #13 Complete

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -10,28 +10,51 @@
 
 
 // O(n)
-function fib(n) {
-      // Always know the first two will be 0, 1
-      const results = [0, 1];
+// function fib(n) {
+//       // Always know the first two will be 0, 1
+//       const results = [0, 1];
 
-      // Increase next item by adding the two pervious items together [5, 8, 13]
-      for (let i = 2; i <= n; i++){
-            let a = results[i - 1];
-            let b = results[i - 2];
+//       // Increase next item by adding the two pervious items together [5, 8, 13]
+//       for (let i = 2; i <= n; i++){
+//             let a = results[i - 1];
+//             let b = results[i - 2];
 
-            // Add together and append to array
-            results.push(a + b);
-      }
-      return results[results.length - 1];
-}
+//             // Add together and append to array
+//             results.push(a + b);
+//       }
+//       return results[results.length - 1];
+// }
 
 
 // O(n)
-function fib(n) {
+function slowFib(n) {
       // n = 0 or n = 1
       if (n < 2) return n;
 
       return fib(n - 1) + fib(n - 2);
 }
+
+
+function memoize(fn) {
+      // init cache of results and args of functions already ran
+      const cache = {};
+
+      return (...args) => {
+            // if function with those params has already ran return cached result
+            if (cache[args]) return cache[args];
+
+            // call function with args and get response using apply
+            const result = fn.apply(this, args);
+
+            // store returned value with key as args
+            cache[args] = result;
+
+            // return that result
+            return result;
+
+      }
+}
+
+const fib = memoize(slowFib);
 
 module.exports = fib;
