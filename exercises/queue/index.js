5b540fdd8f3fd70aad11391fff81772cf70f12ea
Brett Dawidowski <brett@dawi.io>
2019-04-06 16:30:12 -0400

    Updating Queue Class with peek()

--- exercises/queue/index.js
+++ exercises/queue/index.js
@@ -9,7 +9,6 @@
 //     q.remove(); // returns 1;
 
 class Queue {
-
       // Init Base Array as Queue
       constructor() { 
             this.q = [];
@@ -21,6 +20,10 @@ class Queue {
       // Using pop to remove the last item / Array.length - 1
       remove() {
             return this.q.pop();
+      }
+      // Accessing/Getter the next item in the Queue
+      peek() {
+            return this.q[this.q.length - 1];
       }
 }
 
