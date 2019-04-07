bfaec1aceb2351c63b942be781c68f8cac2afb7b
Brett Dawidowski <brett@dawi.io>
2019-04-06 22:28:27 -0400

    [Circular LinkedList] Number 21 Completed

--- exercises/circular/index.js
+++ exercises/circular/index.js
@@ -12,6 +12,27 @@
 //   c.next = b;
 //   circular(l) // true
 
-function circular(list) {}
+function circular(list) {
+      // Init slow and fast to first node
+      let slow = list.getFirst();
+      let fast = list.getFirst();
+
+      // Check to make sure fast.next and fast.next.next exist before
+      // moving the slow and fast down the linkedList
+      while (fast.next && fast.next.next) {
+            // Increase by one Node
+            slow = slow.next;
+            // Increase by two Nodes
+            fast = fast.next.next;
+
+            // When fast node and slow node are referencing the same node
+            // we know its a circular LinkedList and break while loop by
+            // returning true
+            if (slow === fast) return true;
+      }
+      // Found end of LinkedList so return false becuase it is
+      // not a circular LinkedList
+      return false;
+}
 
 module.exports = circular;
