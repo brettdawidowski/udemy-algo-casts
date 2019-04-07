561e13add8d992f3f051cf5adab98dd7111b3907
Brett Dawidowski <brett@dawi.io>
2019-04-06 22:49:54 -0400

    [From End/Last] Number 22 Completed

--- exercises/fromlast/index.js
+++ exercises/fromlast/index.js
@@ -11,6 +11,28 @@
 //    list.insertLast('d');
 //    fromLast(list, 2).data // 'b'
 
-function fromLast(list, n) {}
+function fromLast(list, n) {
+      let slow = list.getFirst();
+      // let fast = list.getAt(n);
+      // OR if getAt isnt avaible iterate before
+      let fast = list.getFirst();
+
+      // Move Fast first n times next
+      for (let i = 0; i < n; i++) {
+            fast = fast.next;
+      }
+      
+      // Until fast finds the end of the LinkedList
+      while (fast.next) {
+            // Increase by one Node
+            slow = slow.next;
+            // Increase by one Nodes
+            fast = fast.next;
+      }
+      // When fast finds the end of the LinkedList
+      // slow will be on the n from the end of the LinkedList
+      return slow;
+
+}
 
 module.exports = fromLast;
