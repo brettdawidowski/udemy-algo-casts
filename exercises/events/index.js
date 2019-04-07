1897552aa23946dc5a49c883e44db46bb50f3704
Brett Dawidowski <brett@dawi.io>
2019-04-07 16:08:30 -0400

    [Events] Number 27 Complete

--- exercises/events/index.js
+++ exercises/events/index.js
@@ -4,16 +4,38 @@
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
-  // Register an event handler
-  on(eventName, callback) {}
+      constructor() {
+            this.events = {}; 
+      }
+	// Register an event handler
+      on(eventName, callback) {
+            // If exist push to list of callbacks
+            if (this.events[eventName]) {
+                  this.events[eventName].push(callback);
+            // If not add eventName as key with array of callback
+            } else {
+                  this.events[eventName] = [callback];
+            }
+      }
 
-  // Trigger all callbacks associated
-  // with a given eventName
-  trigger(eventName) {}
+	// Trigger all callbacks associated
+	// with a given eventName
+      trigger(eventName) {
+            // if this event is registered
+            if (this.events[eventName]) {
+                  // Loop through each callback in array and call
+                  for (let fn of this.events[eventName]) {
+                        fn(); // Call callback function 
+                  }
+            }
+      }
 
-  // Remove all event handlers associated
-  // with the given eventName
-  off(eventName) {}
+	// Remove all event handlers associated
+	// with the given eventName
+      off(eventName) {
+            // Deleting key from object
+            delete this.events[eventName];
+      }
 }
 
 module.exports = Events;
